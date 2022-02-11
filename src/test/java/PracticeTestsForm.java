3d838846b44f6bbbab6fea2918495b6b65ddc367
amstell27 <sergei_i27@mail.ru>
2022-02-11 17:20:50 +0700

    Change import in automationPracticeTests

--- src/test/java/PracticeTestsForm.java
+++ src/test/java/PracticeTestsForm.java
@@ -2,7 +2,7 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.selector.ByText;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
+
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
