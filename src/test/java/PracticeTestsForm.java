4b5ad0f11574978a921651b76840a9752e3202a8
amstell27 <sergei_i27@mail.ru>
2022-02-13 17:11:56 +0700

    Change automationPracticeTests

--- src/test/java/PracticeTestsForm.java
+++ src/test/java/PracticeTestsForm.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -23,25 +24,21 @@ public class PracticeTestsForm {
         open("/automation-practice-form");
 
         $("#firstName").setValue("Alex");
-
         $("#lastName").setValue("Alexeev");
-
         $("#userEmail").setValue("emain@email.com");
-
-        $("[for='gender-radio-1']").click();
-
-        //userNumber
+//        $("[for='gender-radio-1']").click();
+        $(byText("Male")).click();
         $("#userNumber").setValue("8002000500");
 
-        //dateOfBirthInput
         $("#dateOfBirthInput").click();
         $("[class='react-datepicker__month-select']").selectOption("June");
         $("[class='react-datepicker__year-select']").selectOption("1992");
         $("[class='react-datepicker__day react-datepicker__day--025']").click();
-        $("#subjectsInput").setValue("Chemistry").pressEnter();
-        $("[for='hobbies-checkbox-1']").click();
-        $("#uploadPicture").uploadFromClasspath("photo.jpg");
 
+        $("#subjectsInput").setValue("Chemistry").pressEnter();
+//        $("[for='hobbies-checkbox-1']").click();
+        $(byText("Sports")).click();
+        $("#uploadPicture").uploadFromClasspath("photo.jpg");
         $("#currentAddress").setValue("Current Address");
 
         $("#state").click();
@@ -53,8 +50,18 @@ public class PracticeTestsForm {
         $("#submit").click();
 
         $("[class='modal-header']").shouldBe(text("Thanks for submitting the form"));
-        $("[class='table-responsive'").shouldHave(text("Alex"), text("Alexeev"), text("emain@email.com"), text("25 June,1992"),
-                text("Chemistry"), text("Sports"), text("Sports"), text("photo.jpg"), text("Current Address"), text("NCR Delhi"));
+
+        $("[class='table-responsive'").shouldHave(
+                text("Student Name"), text("Alex Alexeev"),
+                text("Student Email"), text("emain@email.com"),
+                text("Gender"), text("Male"),
+                text("Mobile"), text("8002000500"),
+                text("Date of Birth"), text("25 June,1992"),
+                text("Subjects"), text("Chemistry"),
+                text("Hobbies"), text("Sports"),
+                text("Picture"), text("photo.jpg"),
+                text("Address"), text("Current Address"),
+                text("State and City"), text("NCR Delhi"));
 
     }
 
